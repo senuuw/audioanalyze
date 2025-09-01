2c163d53516626ffe087e208071907d548e95518
senuuw <senu9248@gmail.com>
2025-09-01 13:00:02 -0700

    add url arg

--- analyze.py
+++ analyze.py
@@ -1,1 +1,8 @@
-print("hello world")
+import argparse
+import yt_dlp
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', help="Input video url")
+args = parser.parse_args()
+
+print(args.url)
