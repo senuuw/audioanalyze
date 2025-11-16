6f9133be9fbc894c22a4492ccc18fd06967157bf
senuuw <senu9248@gmail.com>
2025-11-16 01:43:40 -0800

    check vod + post comment

--- yt_functions.py
+++ yt_functions.py
@@ -35,15 +35,7 @@ def get_credentials():
     return creds
 
 def check_latest_video():
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "client_secret_1.json"
-
-    # Get credentials and create an API client
-    credentials = get_credentials()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+    youtube = get_youtube_api()
 
     playlist_id_request = youtube.channels().list(
         part="contentDetails",
@@ -61,6 +53,51 @@ def check_latest_video():
     latest_vid_request = latest_vid_request.execute()
     video_id = latest_vid_request['items'][0]['contentDetails']['videoId']
 
+    return video_id
+
+def is_vod(video_id):
+
+    youtube = get_youtube_api()
+    video_request = youtube.videos().list(
+        part="snippet,liveStreamingDetails",
+        id=video_id
+    )
+    video_info = video_request.execute()["items"][0]
+
+    # If liveStreamingDetails exists → it was a livestream (VOD or live)
+    return "liveStreamingDetails" in video_info
+
+def get_youtube_api():
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "client_secret_1.json"
+
+    # Get credentials and create an API client
+    credentials = get_credentials()
+    youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, credentials=credentials)
+
+    return youtube
+
+def post_comment(video_id, comment_text):
+    youtube = get_youtube_api()
+
+    request = youtube.commentThreads().insert(
+        part="snippet",
+        body={
+          "snippet": {
+            "videoId": video_id,
+            "topLevelComment": {
+              "snippet": {
+                "textOriginal": comment_text
+              }
+            }
+          }
+        }
+    )
+    comment_details = request.execute()
+
 
 if __name__ == "__main__":
-    check_latest_video()
+    print(is_vod("RA-oPPqhY6I"))
